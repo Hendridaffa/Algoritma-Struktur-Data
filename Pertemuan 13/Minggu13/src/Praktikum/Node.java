d0abdaab317f39de4f0aae35dc6c29a87359fd80
Hendridaffa <60720676+Hendridaffa@users.noreply.github.com>
2020-04-30 11:40:07 +0700

    Add files via upload

---
+++ Pertemuan 13/Minggu13/src/Praktikum/Node.java
@@ -0,0 +1,17 @@
+package Praktikum;
+
+/**
+ *
+ * @author ASUS ROG
+ */
+public class Node {
+
+    int data;
+    Node prev, next;
+
+    Node(Node prev, int data, Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
