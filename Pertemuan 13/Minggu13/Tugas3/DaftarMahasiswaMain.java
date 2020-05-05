515df0d0e41646c61fa6d4a2d7143027bc133c0a
Hendridaffa <60720676+Hendridaffa@users.noreply.github.com>
2020-05-05 21:03:01 +0700

    Add files via upload

---
+++ Pertemuan 13/Minggu13/Tugas3/DaftarMahasiswaMain.java
@@ -0,0 +1,29 @@
+package Tugas3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS ROG
+ */
+public class DaftarMahasiswaMain {
+
+    public static void main(String[] args) {
+        DaftarMahasiswa data = new DaftarMahasiswa();
+        Scanner sc = new Scanner(System.in);
+        String nama;
+        int nilai;
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Masukkan nama Mahasiswa\t\t: ");
+            nama = sc.nextLine();
+            System.out.print("Masukkan nilai Mahasiswa\t: ");
+            nilai = sc.nextInt();
+            sc.nextLine();
+            data.addLast(nama, nilai);
+            System.out.println("");
+        }
+        data.print();
+        data.SelectionSort();
+        data.print();
+    }
+}
