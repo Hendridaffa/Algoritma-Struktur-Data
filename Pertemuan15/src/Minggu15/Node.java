d393ade0bea8c6f17deb87c4e2846905341ffaa1
Hendridaffa <60720676+Hendridaffa@users.noreply.github.com>
2020-05-15 00:18:19 +0700

    Add files via upload

---
+++ Pertemuan15/src/Minggu15/Node.java
@@ -0,0 +1,18 @@
+package Minggu15;
+
+/**
+ *
+ * @author ASUS ROG
+ */
+public class Node {
+
+    int data;
+    Node prev;
+    Node next;
+
+    Node(Node prev, int data, Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
